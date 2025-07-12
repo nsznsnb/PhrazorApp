7a10f047900e6aa02a25d0d70d374c6737b67528
nsznsnb <nsznsnb@gmail.com>
2025-07-12 17:26:11 +0900

    ログイン処理不具合修正

--- Common/ComMessage.cs
+++ Common/ComMessage.cs
@@ -3,5 +3,7 @@
     public class ComMessage
     {
         public const string REQUIRED_DETAIL = "{0}を入力してください。";
+
+        public const string MSG_E_INVALID_ACCOUNT = "ログインIDまたはパスワードが一致しません。";
     }
 }
