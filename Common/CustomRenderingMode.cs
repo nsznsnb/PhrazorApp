816a15cc9bfe57686faf54352b0a983b877fa74b
nsznsnb <nsznsnb@gmail.com>
2025-07-12 14:58:34 +0900

    レンダーモードクラスの追加

---
+++ Common/CustomRenderingMode.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+
+namespace PhrazorApp.Common
+{
+    /// <summary>
+    /// Blazorのカスタムレンダリングモードを定義するクラス。
+    /// </summary>
+    public static class CustomRenderingMode
+    {
+        public static InteractiveServerRenderMode InteractiveServer { get; } = new();
+        public static InteractiveWebAssemblyRenderMode InteractiveWebAssembly { get; } = new();
+        public static InteractiveAutoRenderMode InteractiveAuto { get; } = new();
+
+        public static InteractiveServerRenderMode InteractiveServerWithourPrerendering { get; } = new(prerender: false);
+        public static InteractiveWebAssemblyRenderMode InteractiveWebAssemblyWithoutPrerendering { get; } = new(prerender: false);
+        public static InteractiveAutoRenderMode InteractiveAutoWithourPrerendering { get; } = new(prerender: false);
+    }
+}
