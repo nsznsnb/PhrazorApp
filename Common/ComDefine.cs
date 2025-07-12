ea1d6b1b6fc254499f7fc34d4069812ffa83e5d6
nsznsnb <nsznsnb@gmail.com>
2025-07-12 14:24:04 +0900

    定義定数・メッセージ定数ファイル追加

---
+++ Common/ComDefine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhraseMudBlazor.Shared.Common
+{
+    public static class ComDefine
+    {
+        public const string LABEL_TABLE_ROWS_PER_PAGE = "1ページの表示件数";
+
+    }
+}
---
+++ Common/ComMessage.cs
@@ -0,0 +1,7 @@
+﻿namespace PhrazorApp.Common
+{
+    public class ComMessage
+    {
+        public const string REQUIRED_DETAIL = "{0}を入力してください。";
+    }
+}
