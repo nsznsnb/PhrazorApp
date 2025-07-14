9a9764a99639166a640fa3ef63515d5e27486c40
nsznsnb <nsznsnb@gmail.com>
2025-07-14 17:25:21 +0900

    全体のthemeをカスタマイズ

--- Common/ComDefine.cs
+++ Common/ComDefine.cs
@@ -4,11 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PhraseMudBlazor.Shared.Common
+namespace PhrazorApp.Common
 {
     public static class ComDefine
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string FONT_FAMILY_1 = "Noto Sans";
+        public const string FONT_FAMILY_2 = "Noto Sans JP";
+        public const string FONT_FAMILY_3 = "sans-serif";
+
+        public const string LABEL_BUTTON_REGISTER = "登録";
+        public const string LABEL_BUTTON_CLEAR = "クリア";
+        public const string LABEL_ROW_ADD = "行追加";
+        public const string LABEL_BUTTON_RETURN_INDEX = "一覧に戻る";
         public const string LABEL_TABLE_ROWS_PER_PAGE = "1ページの表示件数";
+
 
     }
 }
