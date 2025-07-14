190723245b9c38097f3aea4d013a1f2a6b863570
nsznsnb <nsznsnb@gmail.com>
2025-07-14 13:26:57 +0900

    レンダリングモードについてのコメント付与

--- Common/CustomRenderingMode.cs
+++ Common/CustomRenderingMode.cs
@@ -7,12 +7,30 @@ namespace PhrazorApp.Common
     /// </summary>
     public static class CustomRenderingMode
     {
+        /// <summary>
+        /// Serverモード
+        /// </summary>
         public static InteractiveServerRenderMode InteractiveServer { get; } = new();
+        /// <summary>
+        /// Wasmモード
+        /// </summary>
         public static InteractiveWebAssemblyRenderMode InteractiveWebAssembly { get; } = new();
+        /// <summary>
+        /// InteractiveAutoモード(初回ロード時サーバーモード+キャッシュ後Wasmモード)
+        /// </summary>
         public static InteractiveAutoRenderMode InteractiveAuto { get; } = new();
 
+        /// <summary>
+        /// Serverモード(プリレンダリング無し)
+        /// </summary>
         public static InteractiveServerRenderMode InteractiveServerWithourPrerendering { get; } = new(prerender: false);
+        /// <summary>
+        /// Wasmモード(プリレンダリング無し)
+        /// </summary>
         public static InteractiveWebAssemblyRenderMode InteractiveWebAssemblyWithoutPrerendering { get; } = new(prerender: false);
+        /// <summary>
+        /// InteractiveAutoモード(プリレンダリング無し): 初回ロード時サーバーモード+キャッシュ後Wasmモード
+        /// </summary>
         public static InteractiveAutoRenderMode InteractiveAutoWithourPrerendering { get; } = new(prerender: false);
     }
 }
