89ac62d5c2025160a1530c8e65899b0961f12358
nsznsnb <nsznsnb@gmail.com>
2025-07-16 23:50:02 +0900

    メッセージ定数の追加

--- Common/ComMessage.cs
+++ Common/ComMessage.cs
@@ -4,6 +4,9 @@
     {
         public const string REQUIRED_DETAIL = "{0}を入力してください。";
 
+        public const string MSG_E_FAILURE_CREATE_DETAIL = "{0}の登録に失敗しました。";
+        public const string MSG_E_FAILURE_UPDATE_DETAIL = "{0}の修正に失敗しました。";
+        public const string MSG_E_FAILURE_DELETE_DETAIL = "{0}の削除に失敗しました。";
         public const string MSG_E_INVALID_ACCOUNT = "ログインIDまたはパスワードが一致しません。";
     }
 }
