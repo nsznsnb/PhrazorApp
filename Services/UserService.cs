9d53ad2c9e86c08775b4555e0a896c37780c2d1a
nsznsnb <nsznsnb@gmail.com>
2025-07-16 23:50:23 +0900

    ユーザー情報取得サービスの追加

---
+++ Services/UserService.cs
@@ -0,0 +1,39 @@
+﻿using System.Security.Claims;
+
+namespace PhrazorApp.Services
+{
+    public interface IUserService
+    {
+        public string? GetUserId();
+        public string? GetUserName();
+        public ClaimsPrincipal? GetUser();
+    }
+
+    /// <summary>
+    /// ユーザー情報サービス
+    /// </summary>
+    public class UserService : IUserService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UserService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string? GetUserId()
+        {
+            return _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+
+        public string? GetUserName()
+        {
+            return _httpContextAccessor.HttpContext?.User?.Identity?.Name;
+        }
+
+        public ClaimsPrincipal? GetUser()
+        {
+            return _httpContextAccessor.HttpContext?.User;
+        }
+    }
+}
