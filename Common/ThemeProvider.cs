56ac08ad4d393f2294a4975847c85b310e196d37
nsznsnb <nsznsnb@gmail.com>
2025-07-18 05:50:27 +0900

    アプリテーマ設定クラスの追加

---
+++ Common/ThemeProvider.cs
@@ -0,0 +1,36 @@
+﻿using MudBlazor;
+
+namespace PhrazorApp.Common
+{
+    /// <summary>
+    /// アプリのテーマ設定
+    /// </summary>
+    public static class AppTheme
+    {
+        public static readonly MudTheme MyCustomTheme = new MudTheme()
+        {
+            PaletteLight = new PaletteLight()
+            {
+                Primary = Colors.Indigo.Default,
+                Secondary = Colors.Cyan.Default,
+                AppbarBackground = Colors.Indigo.Default,
+                Background = Colors.Gray.Lighten5,
+            },
+            LayoutProperties = new LayoutProperties()
+            {
+                AppbarHeight = "54px"
+            },
+            Typography = new Typography()
+            {
+                Default = new DefaultTypography()
+                {
+                    FontFamily = new[] {
+                    ComDefine.FONT_FAMILY_1,
+                    ComDefine.FONT_FAMILY_2,
+                    ComDefine.FONT_FAMILY_3
+                }
+                }
+            }
+        };
+    }
+}
