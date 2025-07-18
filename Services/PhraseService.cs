a2f6ca168a28210e67fbfd62a9a9558687d95127
nsznsnb <nsznsnb@gmail.com>
2025-07-19 07:59:24 +0900

    フレーズ作成画面・サービス追加

---
+++ Services/PhraseService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhrazorApp.Data;
+using PhrazorApp.ViewModels;
+
+namespace PhrazorApp.Services
+{
+    public interface IPhraseService
+    {
+        public Task<List<PhraseModel>> GetPhraseViewModelListAsync();
+
+        public Task<PhraseModel> GetPhraseViewModelAsync(Guid largePhraseId);
+
+        public Task<IServiceResult> CreatePhraseAsync(PhraseModel model);
+
+        public Task<IServiceResult> UpdatePhraseAsync(PhraseModel model);
+
+        public Task<IServiceResult> DeletePhraseAsync(Guid largePhraseId);
+    }
+    public class PhraseService
+    {
+        private readonly IDbContextFactory<EngDbContext> _dbContextFactory;
+        private readonly IUserService _userService;
+        private readonly ILogger<PhraseService> _logger;
+
+        public PhraseService(IDbContextFactory<EngDbContext> dbContextFactory, IUserService userService, ILogger<PhraseService> logger)
+        {
+            _dbContextFactory = dbContextFactory;
+            _userService = userService;
+            _logger = logger;
+        }
+    }
+}
