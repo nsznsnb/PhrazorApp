8283e0c191d98e12b851ae792e5e501fc404d382
nsznsnb <nsznsnb@gmail.com>
2025-07-19 07:55:12 +0900

    メッセージ定数の追加

--- Common/ComMessage.cs
+++ Common/ComMessage.cs
@@ -6,6 +6,10 @@
         public const string MSG_I_SUCCESS_CREATE_DETAIL = "{0}を作成しました。";
         public const string MSG_I_SUCCESS_UPDATE_DETAIL = "{0}を更新しました。";
         public const string MSG_I_SUCCESS_DELETE_DETAIL = "{0}を削除しました。";
+        public const string MSG_I_SUCCESS_UPLOAD_DETAIL = "{0}をアップロードしました。";
+        public const string MSG_I_SUCCESS_DOWNLOAD_DETAIL = "{0}をダウンロードしました。";
+
+
 
         public const string MSG_I_CONFIRM_REGIST = "{0}を登録します。よろしいですか?";
         public const string MSG_I_CONFIRM_DELETE = "{0}を削除します。よろしいですか?";
@@ -14,6 +18,8 @@
         public const string MSG_E_ERROR_CREATE_DETAIL = "{0}の作成中にエラーが発生しました。";
         public const string MSG_E_ERROR_UPDATE_DETAIL = "{0}の更新中にエラーが発生しました。";
         public const string MSG_E_ERROR_DELETE_DETAIL = "{0}の削除中にエラーが発生しました。";
+        public const string MSG_E_ERROR_EXECUTE_DETAIL = "{0}に失敗しました。";
+        public const string MSG_E_ERROR_EXECUTE_DETAIL2 = "{0}に失敗しました。({1})";
         public const string MSG_E_INVALID_ACCOUNT = "ログインIDまたはパスワードが一致しません。";
     }
 }
