0914a8d4944a2de12796464ebf2905fd483761af
nsznsnb <nsznsnb@gmail.com>
2025-07-19 07:55:40 +0900

    Infoレベルのログ拡張メソッド追加

--- Extensions/LoggerExtensions.cs
+++ Extensions/LoggerExtensions.cs
@@ -5,11 +5,33 @@ namespace PhrazorApp.Extensions
     public static class LoggerExtensions
     {
         /// <summary>
-        /// エラーログ出力：クラス・メソッド名付きログ出力
+        /// インフォメーションログ出力：クラス・メソッド名付きログ出力
         /// </summary>
         /// <param name="logger"></param>
         /// <param name="eventId">イベントId</param>
         /// <param name="ex">エラー情報</param>
+        /// <param name="message">エラーメッセージ概要</param>
+        /// <param name="args"></param>
+        /// <param name="memberName"></param>
+        /// <param name="filePath"></param>
+        public static void LogInformationWithContext(
+            this ILogger logger,
+            EventId eventId,
+            string message,
+            object[] args = null,
+            [CallerMemberName] string memberName = "",
+            [CallerFilePath] string filePath = "")
+        {
+            var className = Path.GetFileNameWithoutExtension(filePath);
+            var prefix = $"[{className}.{memberName}]";
+            logger.LogInformation(eventId, prefix + message, args ?? Array.Empty<object>());
+        }
+
+        /// <summary>
+        /// 警告ログ出力：クラス・メソッド名付きログ出力
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="eventId">イベントId</param>
         /// <param name="message">エラーメッセージ概要</param>
         /// <param name="args"></param>
         /// <param name="memberName"></param>
@@ -25,6 +47,28 @@ namespace PhrazorApp.Extensions
             var className = Path.GetFileNameWithoutExtension(filePath);
             var prefix = $"[{className}.{memberName}]";
             logger.LogWarning(eventId,  prefix + message, args ?? Array.Empty<object>());
+        }
+
+        /// <summary>
+        /// エラーログ出力：クラス・メソッド名付きログ出力
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="eventId">イベントId</param>
+        /// <param name="message">エラーメッセージ概要</param>
+        /// <param name="args"></param>
+        /// <param name="memberName"></param>
+        /// <param name="filePath"></param>
+        public static void LogErrorWithContext(
+            this ILogger logger,
+            EventId eventId,
+            string message,
+            object[] args = null,
+            [CallerMemberName] string memberName = "",
+            [CallerFilePath] string filePath = "")
+        {
+            var className = Path.GetFileNameWithoutExtension(filePath);
+            var prefix = $"[{className}.{memberName}]";
+            logger.LogError(eventId, prefix + message, args ?? Array.Empty<object>());
         }
 
         /// <summary>
