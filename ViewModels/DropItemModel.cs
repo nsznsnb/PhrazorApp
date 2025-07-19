741efdbc226219a970be5eb5e5283bf0900bb1d6
nsznsnb <nsznsnb@gmail.com>
2025-07-19 20:42:37 +0900

    ドラッグアンドドロップコンポーネント用ビューモデル追加

---
+++ ViewModels/DropItemModel.cs
@@ -0,0 +1,10 @@
+﻿namespace PhrazorApp.ViewModels
+{
+    public class DropItemModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Group { get; set; } = string.Empty; // 例: "Large", "Small" ではなく "Primary", "Secondary" など
+        public string DropTarget { get; set; } = string.Empty;
+    }
+}
