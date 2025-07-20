0fdaf10bc5480aa695fff1cd11c5f572215ab372
nsznsnb <nsznsnb@gmail.com>
2025-07-20 17:03:33 +0900

    メッセージ定数変数名変更

--- Common/ComMessage.cs
+++ Common/ComMessage.cs
@@ -2,7 +2,6 @@
 {
     public class ComMessage
     {
-        public const string REQUIRED_DETAIL = "{0}を入力してください。";
         public const string MSG_I_SUCCESS_CREATE_DETAIL = "{0}を作成しました。";
         public const string MSG_I_SUCCESS_UPDATE_DETAIL = "{0}を更新しました。";
         public const string MSG_I_SUCCESS_DELETE_DETAIL = "{0}を削除しました。";
@@ -14,12 +13,14 @@
         public const string MSG_I_CONFIRM_REGIST = "{0}を登録します。よろしいですか?";
         public const string MSG_I_CONFIRM_DELETE = "{0}を削除します。よろしいですか?";
 
-        public const string MSG_E_ERROR_NOT_FOUND = "{0}が見つかりませんでした。";
+        public const string MSG_E_REQUIRED_DETAIL = "{0}を入力してください。";
+        public const string MSG_E_INVALID_FORMAT = "{0}の形式に誤りがあります。";
+        public const string MSG_E_NOT_FOUND = "{0}が見つかりませんでした。";
         public const string MSG_E_ERROR_CREATE_DETAIL = "{0}の作成中にエラーが発生しました。";
         public const string MSG_E_ERROR_UPDATE_DETAIL = "{0}の更新中にエラーが発生しました。";
         public const string MSG_E_ERROR_DELETE_DETAIL = "{0}の削除中にエラーが発生しました。";
-        public const string MSG_E_ERROR_EXECUTE_DETAIL = "{0}に失敗しました。";
-        public const string MSG_E_ERROR_EXECUTE_DETAIL2 = "{0}に失敗しました。({1})";
+        public const string MSG_E_FAILURE_DETAIL = "{0}に失敗しました。";
+        public const string MSG_E_FAILURE_DETAIL2 = "{0}に失敗しました。({1})";
         public const string MSG_E_INVALID_ACCOUNT = "ログインIDまたはパスワードが一致しません。";
     }
 }
