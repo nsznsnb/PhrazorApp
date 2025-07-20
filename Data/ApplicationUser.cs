39b8a886796d4778369dab3cfbaf800f6146fee8
nsznsnb <nsznsnb@gmail.com>
2025-07-20 17:42:44 +0900

    AspNetUserにFirstName、LastNameを追加

--- Data/ApplicationUser.cs
+++ Data/ApplicationUser.cs
@@ -1,10 +1,15 @@
 using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace PhrazorApp.Data
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        [MaxLength(50)]
+        public string? FirstName { get; set; }
+        [MaxLength(50)]
+        public string? LastName { get; set; }
     }
 
 }
