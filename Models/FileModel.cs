49eb0af8ea8e69c75b6273fe539946e9dadb1876
nsznsnb <nsznsnb@gmail.com>
2025-08-09 15:31:25 +0900

    fileモデル追加

---
+++ Models/FileModel.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace PhrazorApp.Models
+{
+    public class FileModel
+    {
+        public string Name { get; set; }
+        public IBrowserFile File { get; set; }
+    }
+
+
+}
