a5b81d91da5487007518914b216a78bf6abf6a79
nsznsnb <nsznsnb@gmail.com>
2025-08-16 14:27:06 +0900

    ・日記タグ選択コンポーネントの追加 ・日記タグサービスの追加

--- Data/Repositories/DiaryTagRepository.cs
+++ Data/Repositories/DiaryTagRepository.cs
@@ -8,5 +8,13 @@ namespace PhrazorApp.Data.Repositories
     {
         public DiaryTagRepository(EngDbContext context) : base(context) { }
 
+        public Task<List<MDiaryTag>> GetAllAsync()
+        {
+            return _context.Set<MDiaryTag>()
+                      .AsNoTracking()
+                      .OrderBy(t => t.TagName)
+                      .ToListAsync();
+        }
+
     }
 }
---
+++ Services/DiaryTagService.cs
@@ -0,0 +1,39 @@
+﻿// Services/DiaryTagService.cs
+using PhrazorApp.Data.UnitOfWork;
+using PhrazorApp.Models;
+
+namespace PhrazorApp.Services
+{
+
+
+    public sealed class DiaryTagService
+    {
+        private readonly UnitOfWork _uow;
+        private readonly UserService _user;
+
+        public DiaryTagService(UnitOfWork uow, UserService user)
+        {
+            _uow = uow; _user = user;
+        }
+
+        public Task<ServiceResult<List<DropItemModel>>> GetDiaryTagDropItemModelListAsync()
+              => _uow.ReadAsync(async r =>
+              {
+                  var uid = _user.GetUserId();
+                  var tags = await r.DiaryTags.GetAllAsync();
+
+                  var list = tags
+                      .Select(t => new DropItemModel
+                      {
+                          Key1 = t.TagId,                  // TagId (Guid)
+                          Key2 = Guid.Empty,            // タグは親なし
+                          Name = t.TagName ?? string.Empty,
+                          DropTarget = DropItemType.UnAssigned
+                      })
+                      .ToList();
+
+                  return ServiceResult.Success(list);
+              });
+    }
+}
+
