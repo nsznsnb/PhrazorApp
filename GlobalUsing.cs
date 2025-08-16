ceb2a4d09fa0b4216ab8d8ccc0a9c21fcc83abc9
nsznsnb <nsznsnb@gmail.com>
2025-08-16 16:36:31 +0900

    ファイル名の変更

--- GlobalUsing.cs
+++
@@ -1,8 +0,0 @@
-﻿global using PhrazorApp.Commons.Constants;
-global using PhrazorApp.Commons.Settings;
-global using PhrazorApp.Commons.Logging;
-global using PhrazorApp.Commons.Results;
-global using PhrazorApp.Commons.Themes;
-global using PhrazorApp.Infrastructure;
-global using PhrazorApp.Utils;
-global using System.Linq;
---
+++ GlobalUsings.cs
@@ -0,0 +1,9 @@
+﻿global using PhrazorApp.Common.Constants;
+global using PhrazorApp.Common.Options;
+global using PhrazorApp.Common.Logging;
+global using PhrazorApp.Common.Results;
+global using PhrazorApp.Common.Validation;
+
+global using PhrazorApp.Infrastructure;
+global using PhrazorApp.Utils;
+global using System.Linq;
